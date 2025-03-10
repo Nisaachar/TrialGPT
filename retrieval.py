7b8b5a08fa59bf4fffb7ea74da65425740542714
nisaachar <thhat.guy.is@gmail.com>
2025-03-10 16:34:16 -0400

    Cleaning Branch

--- retrieval.py
+++ retrieval.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
         medcpt_document_ids,
         bm25_wt=1,
         medcpt_wt=1,
-        top_n=10
+        top_n=5
     )
 
     # print("\nTop documents from hybrid retrieval:")
